ea2dc6ff4efa7511ff100918ec0c4d1f0fef0c34
@Pikhota <s.pikhota@gmail.com>
2018-05-30 18:02:03 +0300

    add order rooms by name

--- Assets/Scripts/LoadData.cs
+++ Assets/Scripts/LoadData.cs
@@ -2,6 +2,7 @@
 using Assets.Scripts.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,23 +17,20 @@ public class LoadData : MonoBehaviour
     public void Awake()
     {
         data = ParseData.ParseXmlFileToObject("DataGames");
-        AddOptionsToDropdown(dropdown);
-        Initialization(dropdown.value);
-    }
-
-    private void AddOptionsToDropdown(Dropdown dropdownObject)
-    {
         List<string> optionsGameList = new List<string>();
         foreach (Game game in data.Roomlist.Games)
         {
             optionsGameList.Add(game.Name);
         }
-        dropdownObject.GetComponent<Dropdown>().AddOptions(optionsGameList);
+        dropdown.GetComponent<Dropdown>().AddOptions(optionsGameList);
+        Initialization(dropdown.value);
     }
 
     private void Initialization(int index)
     {
-        foreach (SuperGame superGame in data.Roomlist.Games[index].Supergame)
+        RoomList roomList = data.Roomlist;
+        
+        foreach (SuperGame superGame in roomList.Games[index].Supergame)
         {
             super.transform.GetChild(1).GetComponent<Text>().text = superGame.Price;
             super.transform.GetChild(2).GetComponentInChildren<Text>().text = superGame.AmountPlayers + " / " + superGame.MaxPlayers;
@@ -43,7 +41,8 @@ public class LoadData : MonoBehaviour
                 new Vector3(Panel.transform.position.x, Panel.transform.position.y, Panel.transform.position.z),
                 Quaternion.identity, Panel.transform);
         }
-        foreach (Room room in data.Roomlist.Games[index].Rooms)
+
+        foreach (Room room in roomList.Games[index].Rooms.OrderBy(room => room.Text))
         {
             simple.transform.GetChild(1).GetComponent<Text>().text = room.Price;
             simple.transform.GetChild(2).GetComponent<Text>().text = room.Text;
@@ -56,6 +55,8 @@ public class LoadData : MonoBehaviour
                 new Vector3(Panel.transform.position.x, Panel.transform.position.y, Panel.transform.position.z),
                 Quaternion.identity, Panel.transform);
         }
+
+
     }
 
     private float GetPercent(float max, float current)
